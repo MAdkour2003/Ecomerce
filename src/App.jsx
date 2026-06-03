490a1bd073ed763566353e4c4c3467fd01ca698e
MAdkour2003 <yehiamadkour2003@gmail.com>
2026-06-03 04:31:06 +0300

    route for single id

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './page/Home';
-import Pro from './page/pro';
-import Details from './page/Details';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./page/Home";
+import Pro from "./page/pro";
+import Details from "./page/Details";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path='/products' element={<Pro />} />
-        <Route path='/products/:id' element={<Details />} />
+        <Route path="/products" element={<Pro />} />
+        <Route path="/detailed/:id" element={<Details />} />
       </Route>
     </Routes>
   );
