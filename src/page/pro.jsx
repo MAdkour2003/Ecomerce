2a42f6aa9bc7124e19dd00bf54692f23dd77c03f
MAdkour2003 <yehiamadkour2003@gmail.com>
2026-06-03 03:33:54 +0300

    test

--- src/page/pro.jsx
+++ src/page/pro.jsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { addItem } from '../store/cartSlice';
-import { getProducts } from '../api/api';
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { addItem } from "../store/cartSlice";
+import { getProducts } from "../api/api";
 
 // Todo: Change route and component name to Products
 const Pro = () => {
   const dispatch = useDispatch();
   const [Products, setProducts] = useState([]);
   const [visableCount, setvisablecount] = useState(10);
-  const [error, seterror] = useState('');
+  const [error, seterror] = useState("");
 
   const fetchproduct = async () => {
     try {
       const data = await getProducts();
       setProducts(data);
     } catch (err) {
-      seterror('falied to load data ');
+      seterror("falied to load data ");
     }
   };
 
@@ -30,30 +30,30 @@ const Pro = () => {
 
   const visiableProducts = Products.slice(0, visableCount);
   return (
-    <div className='p-6 text-center'>
-      <h1 className='text-title font-medium mb-8'>React Shop</h1>
-      {error && <p className='text-error font-bold'>{error}</p>}
-      <div className='flex flex-wrap justify-center gap-5'>
+    <div className="p-6 text-center">
+      <h1 className="text-title font-medium mb-8">React Shop</h1>
+      {error && <p className="text-error font-bold">{error}</p>}
+      <div className="flex flex-wrap justify-center gap-5">
         {visiableProducts.map((Product) => (
           <div
             key={Product.id}
-            className='bg-white text-text1 rounded-lg w-50 p-0.5 shadow hover:translate-1 transition-all duration-300 ease-linear '
+            className="bg-white text-text1 rounded-lg w-50 p-0.5 shadow hover:translate-1 transition-all duration-300 ease-linear "
           >
             <img
-              className=' object-contain w-full h-48 mb-4'
+              className=" object-contain w-full h-48 mb-4"
               src={Product.image}
-              alt=''
+              alt=""
             />
-            <h3 className='font-bold text-sm text-black overflow-hidden'>
+            <h3 className="font-bold text-sm text-black overflow-hidden">
               {Product.title}
             </h3>
-            <p className='text-textid text-sm'>{Product.category}</p>
-            <p className='text-price font-extrabold'>${Product.price}</p>
+            <p className="text-textid text-sm">{Product.category}</p>
+            <p className="text-price font-extrabold">${Product.price}</p>
 
             <div>
               <Link
                 to={`/products/${Product.id}`}
-                className='text-blue-600 text-sm hover:underline mb-1.5'
+                className="text-blue-600 text-sm hover:underline mb-1.5"
               >
                 View Details
               </Link>
@@ -62,7 +62,7 @@ const Pro = () => {
             {/* Todo: Extract into a variable */}
             <button
               onClick={() => dispatch(addItem(Product.id))}
-              className='bg-addcart text-text1 px-4 py-1 rounded-full text-sm hover:bg-addcarthover transition mb-2'
+              className="bg-addcart text-text1 px-4 py-1 rounded-full text-sm hover:bg-addcarthover transition mb-2"
             >
               + Add to Cart
             </button>
@@ -72,7 +72,7 @@ const Pro = () => {
       {visableCount < Products.length && (
         <button
           onClick={showmore}
-          className='text-text1 mt-4 p-2 border-none bg-showmore hover:bg-showmorehover rounded-full text-center w-1/2'
+          className="text-text1 mt-4 p-2 border-none bg-showmore hover:bg-showmorehover rounded-full text-center w-1/2"
         >
           show more
         </button>
