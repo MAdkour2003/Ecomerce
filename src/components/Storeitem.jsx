57127661d49cf3b141f74880a8e41c8c13b6954e
MAdkour2003 <yehiamadkour2003@gmail.com>
2026-06-04 21:20:50 +0300

    add total price ,passes price

--- src/components/Storeitem.jsx
+++ src/components/Storeitem.jsx
@@ -1,22 +1,27 @@
-import { useMemo } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   addItem,
   removeOne,
   removeItem,
   selectCartQuantityById,
-} from '../store/cartSlice';
+  selectCartItemTotals,
+} from "../store/cartSlice";
 
-export default function StoreItem({ id }) {
+export default function StoreItem({ id, price }) {
   const dispatch = useDispatch();
   const selectQuantity = useMemo(() => selectCartQuantityById(id), [id]);
   const quantity = useSelector(selectQuantity);
 
+  const totals = useSelector(selectCartItemTotals);
+
+  const itemTotal = totals[id] || 0;
+
   if (quantity === 0) {
     return (
       <button
-        onClick={() => dispatch(addItem(id))}
-        className='bg-blue-600 text-white px-4 py-1 rounded-full text-sm hover:bg-blue-700 transition'
+        onClick={() => dispatch(addItem({ id, price }))}
+        className="bg-blue-600 text-white px-4 py-1 rounded-full text-sm hover:bg-addcarthover transition"
       >
         + Add to Cart
       </button>
@@ -24,23 +29,28 @@ export default function StoreItem({ id }) {
   }
 
   return (
-    <div className='flex items-center justify-center gap-2 mt-2'>
+    <div className="flex items-center justify-center gap-2 mt-2">
+      <div className="bg-categorycart/10 px-4 py-1.5 rounded-full border border-categorycart/20">
+        <p className="font-bold text-price text-base">
+          ${itemTotal.toFixed(2)}
+        </p>
+      </div>
       <button
         onClick={() => dispatch(removeOne(id))}
-        className='w-8 h-8 rounded-full bg-gray-200 hover:bg-gray-300 font-bold text-sm'
+        className="w-8 h-8 rounded-full bg-text1 hover:bg-bgcolorWH font-bold text-sm"
       >
         -
       </button>
-      <span className='font-bold text-base'>{quantity}</span>
+      <span className="font-bold text-base">{quantity}</span>
       <button
-        onClick={() => dispatch(addItem(id))}
-        className='w-8 h-8 rounded-full bg-gray-200 hover:bg-gray-300 font-bold text-sm'
+        onClick={() => dispatch(addItem({ id, price }))}
+        className="w-8 h-8 rounded-full bg-text1 hover:bg-bgcolorWH font-bold text-sm"
       >
         +
       </button>
       <button
         onClick={() => dispatch(removeItem(id))}
-        className='text-red-500 text-xs ml-1 hover:underline'
+        className="text-remove text-xs ml-1 hover:underline"
       >
         Remove
       </button>
