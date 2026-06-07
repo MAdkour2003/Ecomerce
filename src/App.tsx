0b827de181e516c8195d05a66a460a4871898d11
mahmoud-txg <mahmoud@txglobe.com>
2026-06-07 08:10:04 +0300

    docs: note future auth gating in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import Layout from './components/Layout';
 import Home from './page/Home';
 import Products from './page/Products';
 import Details from './page/Details';
-
+// USer => login (access) (No access to login & signup)
+// No user => signup + login (N0 access to the rest of the app)
 function App() {
   return (
     <Routes>
