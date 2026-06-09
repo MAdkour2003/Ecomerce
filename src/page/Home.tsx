e10d2a3938cb550f9e9a862ed66919022417dfc3
MAdkour2003 <yehiamadkour2003@gmail.com>
2026-06-09 14:44:30 +0300

    fix double quotes to single quotes in Details and Home

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
-    <div className="p-6">
-      <h1 className="mb-5 mr-1.5 text-4xl font-semibold text-sidebar">Home</h1>
+    <div className='p-6'>
+      <h1 className='mb-5 mr-1.5 text-4xl font-semibold text-sidebar'>Home</h1>
     </div>
   );
 }
