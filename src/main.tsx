190441cdf6dc90a4f0b490c09b4d01a759cf18ea
MAdkour2003 <yehiamadkour2003@gmail.com>
2026-06-23 14:14:55 +0300

    route ,usequeryclient  for tanstack brain

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+
+const queryclinet = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryclinet}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
