f8f57bbd0a504fa301f87a53436cc92b16d3935f
MAdkour2003 <yehiamadkour2003@gmail.com>
2026-06-30 02:11:26 +0300

    install sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
-import { Link, useLocation } from 'react-router-dom';
-import { cn } from '../utils';
+import { Link, useLocation } from "react-router-dom";
+import { cn } from "../utils";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -12,23 +12,25 @@ interface NavItem {
 }
 
 const navItems: NavItem[] = [
-  { path: '/', icon: 'fa-home', label: 'Home' },
-  { path: '/products', icon: 'fa-th-large', label: 'Products' },
-  { path: '/profile', icon: 'fa-user', label: 'My Profile' },
+  { path: "/", icon: "fa-home", label: "Home" },
+  { path: "/products", icon: "fa-th-large", label: "Products" },
+  { path: "/profile", icon: "fa-user", label: "My Profile" },
 ];
 
 function Sidebar({ isOpen }: SidebarProps) {
   const location = useLocation();
 
   return (
-    <aside className={cn('bg-sidebar w-50 h-full px-4', isOpen ? 'block' : 'hidden')}>
+    <aside
+      className={cn("bg-sidebar w-50 h-full px-4", isOpen ? "block" : "hidden")}
+    >
       {navItems.map((item) => (
         <Link
           key={item.label}
           to={item.path}
           className={cn(
-            'block p-3.5 text-sidescontent no-underline cursor-pointer hover:bg-sideconthov',
-            location.pathname === item.path && 'bg-sideconthov'
+            "block p-3.5 text-sidescontent no-underline cursor-pointer hover:bg-sideconthov",
+            location.pathname === item.path && "bg-sideconthov",
           )}
         >
           <i className={`fas ${item.icon} mr-2`}></i>
